045ebdbcce29f00327534c486b59a96f88f60250
hasiffirdos <h.asif.firdos@gmail.com>
2022-01-20 12:04:14 +0500

    migrations edited

--- src/main/resources/db/migration/V1.3__create_enrollments_table.sql
+++ src/main/resources/db/migration/V1.3__create_enrollments_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `enrollments`
 (
     `student_id` INT NOT NULL,
     `course_id`  INT NOT NULL,
+    `enrollment_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT `StudentsPkAsFkInEnrollments`
         FOREIGN KEY (`student_id`) REFERENCES students(id),
     CONSTRAINT `CoursesPkAsFkInEnrollments`
