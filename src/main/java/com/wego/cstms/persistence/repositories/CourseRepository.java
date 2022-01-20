4e157ec2d95c617905ed7a4ad6ac57dff5846577
hasiffirdos <h.asif.firdos@gmail.com>
2022-01-20 14:33:46 +0500

    Top Tending Courses Query rewrite

--- src/main/java/com/wego/cstms/persistence/repositories/CourseRepository.java
+++ src/main/java/com/wego/cstms/persistence/repositories/CourseRepository.java
@@ -12,8 +12,12 @@ public interface CourseRepository extends CrudRepository<Course,Integer> {
     List<Course> getTopAllTimes();
 
 
-//    @Query(value = "SELECT c FROM Course c OR")
-    @Query(value = "SELECT c FROM Course c ORDER BY c.views DESC, c.updatedAt DESC")
+    @Query(value = "SELECT CSR.id, CSR.name, CSR.created_at,CSR.updated_at, CSR.price, CSR.rating,CSR.description, CSR.prerequisite, CSR.views\n" +
+            "FROM courses CSR\n" +
+            "JOIN enrollments EN ON CSR.id = EN.course_id\n" +
+            "WHERE MONTH(EN.enrollment_date) = MONTH(CURRENT_DATE()) AND YEAR(EN.enrollment_date) = YEAR(CURRENT_DATE())\n" +
+            "GROUP BY CSR.id\n" +
+            "ORDER BY COUNT(EN.course_id) DESC;", nativeQuery = true)
     List<Course> getTopTrending();
 
 
