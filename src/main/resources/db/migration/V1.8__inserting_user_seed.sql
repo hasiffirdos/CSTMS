6f52b729ed75de182efa3a99885a91759db2964f
hasiffirdos <h.asif.firdos@gmail.com>
2022-02-21 11:08:57 +0500

    Inserting an Admin seed

--- src/main/resources/db/migration/V1.8__inserting_user_seed.sql
+++ src/main/resources/db/migration/V1.8__inserting_user_seed.sql
@@ -1,3 +1,5 @@
-INSERT INTO users (active, password, role, user_name, firstname, lastname, email, phone, signup_date, dob, age)
-VALUES (true, '$2a$10$cqFno5hmsI/JsmR7lUoF7eUgQhFulv6ZN.9GE14irg5v3JM7t6zb2',
+INSERT INTO users (id,active, password, role, user_name, firstname, lastname, email, phone, signup_date, dob, age)
+VALUES (1,true, '$2a$10$cqFno5hmsI/JsmR7lUoF7eUgQhFulv6ZN.9GE14irg5v3JM7t6zb2',
         'ADMIN', 'asiffirdos','asif','firdos','asif@mail.com','+923461398093',CURDATE(),CURDATE(),28);
+
+INSERT INTO admins (id) value (1);
